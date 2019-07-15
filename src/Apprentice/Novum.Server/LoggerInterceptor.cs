0304c25754fe5ca368ebcd601591209c56acfa14
ThomasTTZ <50405782+ThomasTTZ@users.noreply.github.com>
2019-07-15 13:36:38 +0200

    toString

--- src/Apprentice/Novum.Server/LoggerInterceptor.cs
+++ src/Apprentice/Novum.Server/LoggerInterceptor.cs
@@ -3,21 +3,23 @@ using System.Diagnostics;
 using System.Threading.Tasks;
 using Grpc.Core;
 using Grpc.Core.Interceptors;
+using System.Text;
 
 namespace Novum.Server
 {
-     public class LoggerInterceptor : Interceptor
+    public class LoggerInterceptor : Interceptor
     {
 
-    public override async Task<TResponse> UnaryServerHandler<TRequest, TResponse>(TRequest request, ServerCallContext context, UnaryServerMethod<TRequest, TResponse> continuation)
-    {
-        var stopWatch = Stopwatch.StartNew();
-        var response = await base.UnaryServerHandler(request, context, continuation);
-
-        stopWatch.Stop();
-        System.Diagnostics.Debug.WriteLine("request - response took " + stopWatch.ElapsedMilliseconds + "ms");
-
-        return response;
+        public override async Task<TResponse> UnaryServerHandler<TRequest, TResponse>(TRequest request, ServerCallContext context, UnaryServerMethod<TRequest, TResponse> continuation)
+        {
+            var sb = new StringBuilder();
+            sb.Append("|Request|").Append(request.ToString());
+            Nt.Logging.Log.Communication.Info(sb.ToString());
+            var response = await base.UnaryServerHandler(request, context, continuation);
+            sb = new StringBuilder();
+            sb.Append("|Response|").Append(response.ToString());
+            Nt.Logging.Log.Communication.Info(sb.ToString());
+            return response;
+        }
     }
-  }
 }
