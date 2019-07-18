db2d6de1685f3735dc3ae797d7af70ba847de306
ThomasTTZ <50405782+ThomasTTZ@users.noreply.github.com>
2019-07-18 16:39:45 +0200

    added pinpad + logo img

--- src/Apprentice/Novum.Client/novum_client/lib/pinpad.dart
+++ src/Apprentice/Novum.Client/novum_client/lib/pinpad.dart
@@ -12,9 +12,6 @@ class Client extends StatelessWidget {
     SystemChrome.setEnabledSystemUIOverlays([]);
     return MaterialApp(
       title: 'novum_client',
-      theme: ThemeData(
-        primarySwatch: Colors.grey,
-      ),
       home: MyHomePage(title: 'initialize screen'),
     );
   }
@@ -33,207 +30,259 @@ class PinPad extends State<MyHomePage> {
   Widget build(BuildContext context) {
     print("launched PinPad");
     double width = MediaQuery.of(context).size.width;
-    return new Column(
-      
-      crossAxisAlignment: CrossAxisAlignment.end,
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: <Widget>[
-        Column(
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    ButtonTheme(
-                      minWidth: width / 3,
-                      height: 75.0,
-                      child: RaisedButton(
-                        shape: new ContinuousRectangleBorder(),
-                        onPressed: () {},
-                        child: Text(
-                          "1",
-                          style: TextStyle(fontSize: 17),
-                        ),
-                      ),
-                    ),
-                    ButtonTheme(
-                      minWidth: width / 3,
-                      height: 75.0,
-                      child: RaisedButton(
-                        shape: new ContinuousRectangleBorder(),
-                        onPressed: () {},
-                        child: Text(
-                          "2",
-                          style: TextStyle(fontSize: 17),
-                        ),
-                      ),
-                    ),
-                    ButtonTheme(
-                      minWidth: width / 3,
-                      height: 75.0,
-                      child: RaisedButton(
-                        shape: new ContinuousRectangleBorder(),
-                        onPressed: () {},
-                        child: Text(
-                          "3",
-                          style: TextStyle(fontSize: 17),
-                        ),
-                      ),
-                    ),
-                  ],
+
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          new Container(
+              width: width,
+              height:  MediaQuery.of(context).size.height / 3,
+              decoration: new BoxDecoration(
+                image: new DecorationImage(
+                  image: ExactAssetImage('assets/NovaTouch-logo.jpg'),
+                  fit: BoxFit.cover,
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    ButtonTheme(
-                      minWidth: width / 3,
-                      height: 75.0,
-                      child: RaisedButton(
-                        shape: new ContinuousRectangleBorder(),
-                        onPressed: () {},
-                        child: Text(
-                          "4",
-                          style: TextStyle(fontSize: 17),
-                        ),
-                      ),
-                    ),
-                    ButtonTheme(
-                      minWidth: width / 3,
-                      height: 75.0,
-                      child: RaisedButton(
-                        shape: new ContinuousRectangleBorder(),
-                        onPressed: () {},
-                        child: Text(
-                          "5",
-                          style: TextStyle(fontSize: 17),
-                        ),
-                      ),
-                    ),
-                    ButtonTheme(
-                      minWidth: width / 3,
-                      height: 75.0,
-                      child: RaisedButton(
-                        shape: new ContinuousRectangleBorder(),
-                        onPressed: () {},
-                        child: Text(
-                          "6",
-                          style: TextStyle(fontSize: 17),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    ButtonTheme(
-                      minWidth: width / 3,
-                      height: 75.0,
-                      child: RaisedButton(
-                        shape: new ContinuousRectangleBorder(),
-                        onPressed: () {},
-                        child: Text(
-                          "7",
-                          style: TextStyle(fontSize: 17),
-                        ),
-                      ),
-                    ),
-                    ButtonTheme(
-                      minWidth: width / 3,
-                      height: 75.0,
-                      child: RaisedButton(
-                        shape: new ContinuousRectangleBorder(),
-                        onPressed: () {},
-                        child: Text(
-                          "8",
-                          style: TextStyle(fontSize: 17),
-                        ),
-                      ),
-                    ),
-                    ButtonTheme(
-                      minWidth: width / 3,
-                      height: 75.0,
-                      child: RaisedButton(
-                        shape: new ContinuousRectangleBorder(),
-                        onPressed: () {},
-                        child: Text(
-                          "9",
-                          style: TextStyle(fontSize: 17),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    ButtonTheme(
-                      minWidth: width / 3,
-                      height: 75.0,
-                      child: RaisedButton(
-                        shape: new ContinuousRectangleBorder(),
-                        onPressed: () {},
-                        child: Text(
-                          "",
-                          style: TextStyle(fontSize: 17),
-                        ),
-                      ),
-                    ),
-                    ButtonTheme(
-                      minWidth: width / 3,
-                      height: 75.0,
-                      child: RaisedButton(
-                        shape: new ContinuousRectangleBorder(),
-                        onPressed: () {},
-                        child: Text(
-                          "0",
-                          style: TextStyle(fontSize: 17),
-                        ),
-                      ),
-                    ),
-                    ButtonTheme(
-                      minWidth: width / 3,
-                      height: 75.0,
-                      child: RaisedButton(
-                          shape: new ContinuousRectangleBorder(),
-                          onPressed: () {},
-                          child: Column(
-                            children: <Widget>[Icon(Icons.backspace)],
-                          )),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            Row(
-              mainAxisSize: MainAxisSize
-                  .min, // this will take space as minimum as posible(to center)
-              children: <Widget>[
-                ButtonTheme(
-                  buttonColor: Colors.yellow,
-                  minWidth: width / 2,
-                  height: 65.0,
-                  child: RaisedButton(
-                    onPressed: () {},
-                    child: Text("Funktion"),
+              )),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Container(
+                color: Color.fromRGBO(200, 200, 200, 1.0),
+                width: width,
+                height: MediaQuery.of(context).size.height / 12,
+                child: Center(
+                  child: Text(
+                    "PIN eingeben",
+                    style: TextStyle(
+                        fontSize: 24,
+                        color: Color.fromRGBO(120, 120, 120, 1.0)),
+                    textAlign: TextAlign.center,
                   ),
                 ),
-                ButtonTheme(
-                  buttonColor: Colors.yellow,
-                  minWidth: width / 2,
-                  height: 65.0,
-                  child: RaisedButton(
-                    onPressed: () {},
-                    child: Text("OK"),
+              ),
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ButtonTheme(
+                    minWidth: width / 3,
+                    height: 75.0,
+                    child: RaisedButton(
+                      shape: new ContinuousRectangleBorder(),
+                      textColor: Colors.white,
+                      onPressed: () {},
+                      child: Text(
+                        "1",
+                        style: TextStyle(fontSize: 17),
+                      ),
+                      color: Color.fromRGBO(100, 100, 100, 1.0),
+                    ),
                   ),
+                  ButtonTheme(
+                    minWidth: width / 3,
+                    height: 75.0,
+                    child: RaisedButton(
+                      shape: new ContinuousRectangleBorder(),
+                      textColor: Colors.white,
+                      onPressed: () {},
+                      child: Text(
+                        "2",
+                        style: TextStyle(fontSize: 17),
+                      ),
+                      color: Color.fromRGBO(100, 100, 100, 1.0),
+                    ),
+                  ),
+                  ButtonTheme(
+                    minWidth: width / 3,
+                    height: 75.0,
+                    child: RaisedButton(
+                      shape: new ContinuousRectangleBorder(),
+                      textColor: Colors.white,
+                      onPressed: () {},
+                      child: Text(
+                        "3",
+                        style: TextStyle(fontSize: 17),
+                      ),
+                      color: Color.fromRGBO(100, 100, 100, 1.0),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ButtonTheme(
+                    minWidth: width / 3,
+                    height: 75.0,
+                    child: RaisedButton(
+                      shape: new ContinuousRectangleBorder(),
+                      textColor: Colors.white,
+                      onPressed: () {},
+                      child: Text(
+                        "4",
+                        style: TextStyle(fontSize: 17),
+                      ),
+                      color: Color.fromRGBO(100, 100, 100, 1.0),
+                    ),
+                  ),
+                  ButtonTheme(
+                    minWidth: width / 3,
+                    height: 75.0,
+                    child: RaisedButton(
+                      shape: new ContinuousRectangleBorder(),
+                      textColor: Colors.white,
+                      onPressed: () {},
+                      child: Text(
+                        "5",
+                        style: TextStyle(fontSize: 17),
+                      ),
+                      color: Color.fromRGBO(100, 100, 100, 1.0),
+                    ),
+                  ),
+                  ButtonTheme(
+                    minWidth: width / 3,
+                    height: 75.0,
+                    child: RaisedButton(
+                      shape: new ContinuousRectangleBorder(),
+                      textColor: Colors.white,
+                      onPressed: () {},
+                      child: Text(
+                        "6",
+                        style: TextStyle(fontSize: 17),
+                      ),
+                      color: Color.fromRGBO(100, 100, 100, 1.0),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ButtonTheme(
+                    minWidth: width / 3,
+                    height: 75.0,
+                    child: RaisedButton(
+                      shape: new ContinuousRectangleBorder(),
+                      textColor: Colors.white,
+                      onPressed: () {},
+                      child: Text(
+                        "7",
+                        style: TextStyle(fontSize: 17),
+                      ),
+                      color: Color.fromRGBO(100, 100, 100, 1.0),
+                    ),
+                  ),
+                  ButtonTheme(
+                    minWidth: width / 3,
+                    height: 75.0,
+                    child: RaisedButton(
+                      shape: new ContinuousRectangleBorder(),
+                      textColor: Colors.white,
+                      onPressed: () {},
+                      child: Text(
+                        "8",
+                        style: TextStyle(fontSize: 17),
+                      ),
+                      color: Color.fromRGBO(100, 100, 100, 1.0),
+                    ),
+                  ),
+                  ButtonTheme(
+                    minWidth: width / 3,
+                    height: 75.0,
+                    child: RaisedButton(
+                      shape: new ContinuousRectangleBorder(),
+                      textColor: Colors.white,
+                      onPressed: () {},
+                      child: Text(
+                        "9",
+                        style: TextStyle(fontSize: 17),
+                      ),
+                      color: Color.fromRGBO(100, 100, 100, 1.0),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ButtonTheme(
+                    minWidth: width / 3,
+                    height: 75.0,
+                    child: RaisedButton(
+                      shape: new ContinuousRectangleBorder(),
+                      textColor: Colors.white,
+                      onPressed: () {},
+                      child: Text(
+                        "",
+                        style: TextStyle(fontSize: 17),
+                      ),
+                      color: Color.fromRGBO(100, 100, 100, 1.0),
+                    ),
+                  ),
+                  ButtonTheme(
+                    minWidth: width / 3,
+                    height: 75.0,
+                    child: RaisedButton(
+                      shape: new ContinuousRectangleBorder(),
+                      textColor: Colors.white,
+                      onPressed: () {},
+                      child: Text(
+                        "0",
+                        style: TextStyle(fontSize: 17),
+                      ),
+                      color: Color.fromRGBO(100, 100, 100, 1.0),
+                    ),
+                  ),
+                  ButtonTheme(
+                    minWidth: width / 3,
+                    height: 75.0,
+                    child: RaisedButton(
+                        color: Color.fromRGBO(100, 100, 100, 1.0),
+                        shape: new ContinuousRectangleBorder(),
+                        textColor: Colors.white,
+                        onPressed: () {},
+                        child: Column(
+                          children: <Widget>[Icon(Icons.backspace)],
+                        )),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Row(
+            mainAxisSize: MainAxisSize
+                .min, // this will take space as minimum as posible(to center)
+            children: <Widget>[
+              ButtonTheme(
+                buttonColor: Colors.yellow,
+                minWidth: width / 2,
+                height: 65.0,
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text("Funktion"),
                 ),
-              ],
-            ),
-          ],
-        ),
-      ],
+              ),
+              ButtonTheme(
+                buttonColor: Colors.yellow,
+                minWidth: width / 2,
+                height: 65.0,
+                child: RaisedButton(
+                  shape: new ContinuousRectangleBorder(),
+                  onPressed: () {},
+                  child: Text("OK"),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
