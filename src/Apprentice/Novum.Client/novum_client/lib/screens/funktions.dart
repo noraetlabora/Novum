a32caaad8f5a286477f58f9da3de149c5775a037
ThomasTTZ <50405782+ThomasTTZ@users.noreply.github.com>
2019-07-24 10:57:45 +0200

    funktionsWidget/appbar: some layout perfections

--- src/Apprentice/Novum.Client/novum_client/lib/screens/funktions.dart
+++ src/Apprentice/Novum.Client/novum_client/lib/screens/funktions.dart
@@ -13,28 +13,37 @@ class FClient extends StatelessWidget {
     double heigth = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: PreferredSize(
-      preferredSize: Size.fromHeight(heigth*0.095),
-      child: AppBar(
-        backgroundColor: Colors.grey[400],
-        title: Text("Information"),
-      ),
+        preferredSize: Size.fromHeight(heigth * 0.095),
+        child:Container(
+          color: Colors.grey[700],
+  	      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+        AppBar(
+          backgroundColor: Colors.grey[700],
+          title: Text("Information", style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.w300)),
+        ),
+        ],
+        ),
+        ),
       ),
       body: Column(
         mainAxisSize: MainAxisSize.min,
         mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
           Container(
-          height: heigth*0.813,
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 18),
-            child: new DeviceInfo(),
-          ),
+            height: heigth * 0.813,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 18),
+              child: new DeviceInfo(),
+            ),
           ),
           Row(
             crossAxisAlignment: CrossAxisAlignment.end,
             children: <Widget>[
               ButtonTheme(
-                buttonColor: Colors.yellow[850],
+                buttonColor: Colors.yellow,
                 minWidth: width,
                 height: heigth * 0.0915,
                 child: RaisedButton(
