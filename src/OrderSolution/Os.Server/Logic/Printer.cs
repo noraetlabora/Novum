978f73f82546347933d84825ce65c95fff46f0ca
noraetlabora <47247493+noraetlabora@users.noreply.github.com>
2019-11-11 10:58:55 +0100

    comments Printer.cs

--- src/OrderSolution/Os.Server/Logic/Printer.cs
+++ src/OrderSolution/Os.Server/Logic/Printer.cs
@@ -17,11 +17,12 @@ namespace Os.Server.Logic
             var printJobId = DB.Api.Printer.GetPrintJobId(session);
             while (!string.IsNullOrEmpty(printJobId))
             {
+                //get printing data and post the data to the client api
                 var printData = DB.Api.Printer.GetPrintData(session, printJobId);
-
+                Client.ClientApi.Print.PostPrintJob(session, printData);
+                //delete current print job id and get next one
                 DB.Api.Printer.DeletePrintJobId(session, printJobId);
                 printJobId = DB.Api.Printer.GetPrintJobId(session);
-                Client.ClientApi.Print.PostPrintJob(session, printData);
             }
         }
 
